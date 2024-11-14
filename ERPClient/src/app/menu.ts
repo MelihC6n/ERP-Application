26f3234c1cdc997d07285e0428ca5d0d739f54bf
Melih Can Akgüneş <melihcanakgunes@gmail.com>
2024-11-14 12:11:54 +0300

    http,swal-toast and error service added; menu pipe added

---
+++ ERPClient/src/app/menu.ts
@@ -0,0 +1,16 @@
+export class MenuModel{
+    name:string="";
+    icon?:string="";
+    url?:string="";
+    isTitle:boolean=false;
+    subMenus?:MenuModel[]=[];
+};
+
+export const Menus:MenuModel[]=[
+    {
+        name:"Ana Sayfa",
+        icon:"far fa fa-home",
+        url:"/",
+        isTitle:false,
+    }
+];
