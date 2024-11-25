1394dfe840aa26626056f133850a3e1c912aeb86
Melih Can Akgüneş <melihcanakgunes@gmail.com>
2024-11-25 17:53:34 +0300

    recipe controller updated;

---
+++ ERPServer/ERPServer.WebAPI/Controller/RecipeController.cs
@@ -0,0 +1,36 @@
+﻿using ERPServer.Application.Features.Recipes.CreateRecipe;
+using ERPServer.Application.Features.Recipes.DeleteRecipe;
+using ERPServer.Application.Features.Recipes.GetAllRecipe;
+using ERPServer.WebAPI.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERPServer.WebAPI.Controller;
+
+public class RecipeController : ApiController
+{
+    public RecipeController(IMediator mediator) : base(mediator)
+    {
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateRecipeCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> GetAll(GetAllRecipeQuery request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Delete(DeleteRecipeCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request);
+        return StatusCode(response.StatusCode, response);
+    }
+}
