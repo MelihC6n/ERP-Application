1eaf73d1e27cf203f057b05e1c23375c64083af4
Melih Can Akgüneş <melihcanakgunes@gmail.com>
2024-11-25 17:50:25 +0300

    recipe delete added;

---
+++ ERPServer/ERPServer.Application/Features/Recipes/DeleteRecipe/DeleteRecipeCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Recipes.DeleteRecipe;
+public sealed record DeleteRecipeCommand(
+    Guid Id) : IRequest<Result<string>>;
---
+++ ERPServer/ERPServer.Application/Features/Recipes/DeleteRecipe/DeleteRecipeCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using TS.Result;
+
+namespace ERPServer.Application.Features.Recipes.DeleteRecipe;
+
+internal sealed class DeleteRecipeCommandHandler(
+    IRecipeRepository recipeRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<DeleteRecipeCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(DeleteRecipeCommand request, CancellationToken cancellationToken)
+    {
+        Recipe recipe = await recipeRepository.GetByExpressionAsync(r => r.Id == request.Id, cancellationToken);
+
+        if (recipe == null)
+        {
+            return Result<string>.Failure("Reçete bulunamadı!");
+        }
+
+        recipeRepository.Delete(recipe);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "Reçete başarıyla silindi.";
+    }
+}
