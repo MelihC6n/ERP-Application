509f8974b7b0d1f7b90b9597cad40042196882be
Melih Can Akgüneş <melihcanakgunes@gmail.com>
2024-11-25 17:16:54 +0300

    recipe getall added;

--- ERPServer/ERPServer.Domain/Entities/Recipe.cs
+++ ERPServer/ERPServer.Domain/Entities/Recipe.cs
@@ -4,5 +4,6 @@ namespace ERPServer.Domain.Entities;
 public sealed class Recipe : Entity
 {
     public Guid ProductId { get; set; }
+    public Product? Product { get; set; }
     ICollection<RecipeDetail>? RecipeDetails { get; set; }
 }
